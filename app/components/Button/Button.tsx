b36f8b196d8beb8b588ebb3591e3c23ca2db71ad
evgen-web-dev <217228725+evgen-web-dev@users.noreply.github.com>
2025-07-16 22:26:24 +0300

    feat(ui): add base ui for desktop header / footer

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import { ComponentProps, PropsWithChildren, ReactNode } from "react";
+import { type Button, } from "./types";
+
+  
+
+type ButtonProps = PropsWithChildren<Button> & ComponentProps<'button' | 'a'>;
+
+export default function Button({ ...props }: ButtonProps) {
+
+    let classes = `${props.className || ''} button button--${props?.styleType || 'primary'} font-medium py-3 px-7 anchor-button-focus`;
+    
+    if (props.buttonType === "anchor") {
+       return <a className={classes} href={props.href} title={props.title}>{props.children}</a>
+    } 
+
+    return <button className={classes} type={props.actionType} title={props.title}>{props.children}</button>
+}
