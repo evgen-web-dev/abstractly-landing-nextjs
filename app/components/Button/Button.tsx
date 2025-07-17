c93321cbeaee04618ad66a6a719b4b66f1aa38c6
evgen-web-dev <217228725+evgen-web-dev@users.noreply.github.com>
2025-07-17 14:33:18 +0300

    feat(ui): add base mobile header / footer

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = PropsWithChildren<Button> & ComponentProps<'button' | 'a'>;
 
 export default function Button({ ...props }: ButtonProps) {
 
-    let classes = `${props.className || ''} button button--${props?.styleType || 'primary'} font-medium py-3 px-7 anchor-button-focus`;
+    let classes = `${props.className || ''} button button--${props?.styleType || 'primary'} font-medium py-3 px-7 anchor-button-focus whitespace-nowrap`;
     
     if (props.buttonType === "anchor") {
        return <a className={classes} href={props.href} title={props.title}>{props.children}</a>
